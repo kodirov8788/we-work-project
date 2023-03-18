be0f8004e940e46fc348d825ea42c812b4c4b006
kodirov8788 <kodirov8788@gmail.com>
2023-03-18 14:45:44 +0500

    sadas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import './App.css';
-<<<<<<< HEAD
 import IdeasBy from './components/IdeasBy';
-=======
 import Navbar from './components/Navbar';
->>>>>>> origin/sam
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+
       <IdeasBy />
       <a href="/GlobalNetwork">Global Network</a>
-      <h1>Hello</h1>
-      <h1>
-        <Navbar />
-      </h1>
     </div>
   );
 }
