ce3362952db8f434a5c817258661c928bf79bee6
kodirov8788 <kodirov8788@gmail.com>
2023-03-18 14:20:13 +0500

    dsfsd

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import IdeasBy from './components/IdeasBy';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       <IdeasBy />
-=======
+
       <h1>Hello</h1>
->>>>>>> fcd579fa65480d58cd6a66bc8c1d97699fee0441
+
     </div>
   );
 }
